98c42ff8c91081d25372ec1ae4e337f7508f4985
Guna <gunasrini0106@gmail.com>
2024-05-04 21:58:49 +0530

    commit changes for layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import{HeaderComponent} from './header/header.component'
+import{MainSecComponent} from './main-sec/main-sec.component'
+import{SidebarComponent} from './sidebar/sidebar.component'
 import{FooterComponent} from './footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainSecComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main-sec/main-sec.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-sec',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-sec.component.html',
+  styleUrl: './main-sec.component.css'
+})
+export class MainSecComponent {
+
+}
